d5fd56827a8add8326a15dfc7a629cde04f9a34d
alpha34727 <alpha34727@gmail.com>
2024-03-27 21:50:35 +0800

    BisectionMethod

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ x = sp.symbols('x')
 
 f = x ** 3 + 2 * x - 5
 area = (sp.Number(1), sp.Number(2))
-n = 50
+n = 10
 
 #牛頓法（右逼近）
 def NewtonMethodRight(f, area, evaluate):
@@ -22,14 +22,34 @@ def NewtonMethodLeft(f, area, evaluate):
     return an.evalf(n), area[1]
 
 #二分逼近法
-def BisectionMethod(f, a, evalutate):
-    pass
+def BisectionMethod(f, area, evalutate):
+    amid = (area[0] + area[1]) / 2
+    if f.subs(x, area[0]) > f.subs(x, area[1]):
+        if f.subs(x, amid) > 0:
+            area = (amid, area[1])
+        else:
+            area = (area[0], amid)
+    else:
+        if f.subs(x, amid) > 0:
+            area = (area[0], amid)
+        else:
+            area = (amid, area[1])
+    
+    if evalutate:
+        return area[0].evalf(n), area[1].evalf(n)
+    return area
 
-for i in range(10):
+
+i = 1
+print(f'a1 : ({area[0].evalf(n)}, {area[1].evalf(n)})')
+while True:
     original_area = area
     print(f'a{i+1} : ({area[0].evalf(n)}, {area[1].evalf(n)})')
-    area = NewtonMethodLeft(f, area, True)
+    
+    area = NewtonMethodLeft(f, area, False)
 
-    if str(original_area[0]) == str(area[0]) and str(original_area[1]) == str(area[1]):
+    if str(original_area[0].evalf(n)) == str(area[0].evalf(n)) and str(original_area[1].evalf(n)) == str(area[1].evalf(n)):
         break
+
+    i += 1
     
